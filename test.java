1041d3bfb913faca3e2dfab36952e532e63e9919
z3lop <86207500+z3lop@users.noreply.github.com>
2021-06-21 11:50:48 +0200

    Update and rename README.md to test.java

--- test.java
+++ test.java
@@ -2,9 +2,5 @@ class Main {
     public static void main(Sting[] args) {
         String a = "hello world, this is a test repository";
         System.out.println(a);
-        int a = 5;
-        int b = 6;
-        int c = a + b;
-        System.out.println(c);
     }
 }
